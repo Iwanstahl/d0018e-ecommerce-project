072db92d46f8880b30b8b2453ab0609614d92c23
Geram Grigoryan <geramgrigoryan@4e:aa:13:3a:8b:e0.home>
2026-02-11 23:40:34 +0100

    basic fil struktur

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
 
-URL_DATABASE = "mysql+mysqlconnector://root:root@localhost:3306/fastapi_db"
+URL_DATABASE = os.getenv("URL_DATABASE")
+
+
 
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
