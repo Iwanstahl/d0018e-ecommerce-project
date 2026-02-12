b97878152055882289008199da2aab6cd086b87e
Geram Grigoryan <geramgrigoryan@4e:aa:13:3a:8b:e0.home>
2026-02-12 12:48:56 +0100

    La till oauth2

---
+++ backend/oauth2.py
@@ -0,0 +1,5 @@
+
+
+#Detta kommer att vara vår OAuth2-implementation för autentisering och auktorisering i vårt projekt.
+#Vi kommer att använda OAuth2 med Password (and hashing), Bearer tokens och JWT (JSON Web Tokens) för att hantera användarautentisering och auktorisering på ett säkert sätt.
+#Vi kommer att skapa endpoints för att registrera användare, logga in och hantera tokens, samt skydda våra API-endpoints med OAuth2. Detta kommer att säkerställa att endast auktoriserade användare kan komma åt vissa resurser och utföra specifikac
